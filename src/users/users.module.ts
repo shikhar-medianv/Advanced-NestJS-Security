066ca6b8f4cac2ded6643e815b824370bd2abf89
Shikhar Negi <shikharnegi31@gmail.com>
2026-02-26 16:19:51 +0530

    Learned Custom, Factory Providers, async Providers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './users.conroller';
 import { UserService } from './users.service';
+import { ConfigService } from '@nestjs/config';
+
 
 @Module({
     controllers: [UserController],
     providers: [UserService],
+    // Custom and Async Providers
+    // providers: [UserService, {
+    //     provide: 'DB_CONNECTION',
+    //     useFactory: async (configService: ConfigService) => {
+    //         return createConnection({
+    //             type: 'postgres',
+    //             host: configService.get('DB_HOST'),
+    //             port: 5432,
+    //             username: 'user',
+    //             password: 'password',
+    //             database: 'test',
+    //         });
+    //     },
+    //     inject: [ConfigService],
+    // },],
+
+
 })
 export class UsersModule {
 
