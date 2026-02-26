01626de104d3f8b290675b368c86d67b0ee3e579
Shikhar Negi <shikharnegi31@gmail.com>
2026-02-26 11:12:35 +0530

    Initial commit - NestJs setup with users module

---
+++ src/users/users.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './users.conroller';
+import { UserService } from './users.service';
+
+@Module({
+    controllers: [UserController],
+    providers: [UserService],
+})
+export class UsersModule {
+
+}
